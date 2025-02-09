0bd02166cdff139b746983fb6a56258920ba7069
Jamielrey <124105310+Jamielrey@users.noreply.github.com>
2025-02-09 17:46:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const { serveHTTP } = require('stremio-addon-sdk');
 const builder = require('./handlers');
 
-serveHTTP(builder.getInterface(), { port: 3000 });
+// Usa la porta dinamica assegnata da Render
+const PORT = process.env.PORT || 3000;
+
+serveHTTP(builder.getInterface(), { port: PORT });
+
+console.log(`Addon in esecuzione sulla porta ${PORT}`);
