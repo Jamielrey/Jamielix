258b91b09af0656793ec8436688ba54efdfd0739
Jamielrey <124105310+Jamielrey@users.noreply.github.com>
2025-02-09 19:03:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const { serveHTTP } = require('stremio-addon-sdk');
 const builder = require('./handlers');
 
-const PORT = process.env.PORT || 3000;
-
-// Modifica qui: aggiungi 0.0.0.0 per permettere accessi esterni
-serveHTTP(builder.getInterface(), { port: PORT, address: "0.0.0.0" });
-
-console.log(`Addon in esecuzione sulla porta ${PORT}`);
+const port = process.env.PORT || 4000;
+serveHTTP(builder.getInterface(), { port });
